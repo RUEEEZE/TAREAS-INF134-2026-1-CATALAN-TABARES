d8931c1afb448d790a89f479bd433f372e678261
Fran-cis-cap <franciscac0312@gmail.com>
2026-06-14 00:33:00 +0000

    Avances iniciales tarea 3

---
+++ main.cpp
@@ -0,0 +1,204 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+/* ****
+ * Struct Nodo
+ ******
+ * Nodo para lista de adyacencia
+ ******
+ * Input:
+ * ninguno
+ ******
+ * Returns:
+ * ninguno
+ **** */
+struct Nodo {
+    int destino;
+    Nodo* siguiente;
+
+    Nodo(int d) {
+        destino = d;
+        siguiente = nullptr;
+    }
+};
+
+/* ****
+ * Class Grafo
+ ******
+ * Almacena edificios y dependencias
+ **** */
+class Grafo {
+private:
+    int cantidadEdificios;
+    int* tiempos;
+    Nodo** adyacencia;
+
+public:
+
+    Grafo(int n) {
+        cantidadEdificios = n;
+
+        tiempos = new int[n + 1];
+
+        adyacencia = new Nodo*[n + 1];
+
+        for(int i = 0; i <= n; i++) {
+            adyacencia[i] = nullptr;
+            tiempos[i] = 0;
+        }
+    }
+
+    ~Grafo() {
+
+        for(int i = 1; i <= cantidadEdificios; i++) {
+
+            Nodo* actual = adyacencia[i];
+
+            while(actual != nullptr) {
+                Nodo* borrar = actual;
+                actual = actual->siguiente;
+                delete borrar;
+            }
+        }
+
+        delete[] adyacencia;
+        delete[] tiempos;
+    }
+
+    void cambiarTiempo(int edificio, int nuevoTiempo) {
+        tiempos[edificio] = nuevoTiempo;
+    }
+
+    int obtenerTiempo(int edificio) {
+        return tiempos[edificio];
+    }
+
+    void agregarArista(int origen, int destino) {
+
+        Nodo* nuevo = new Nodo(destino);
+
+        nuevo->siguiente = adyacencia[origen];
+
+        adyacencia[origen] = nuevo;
+    }
+
+    void imprimirGrafo() {
+
+        for(int i = 1; i <= cantidadEdificios; i++) {
+
+            cout << i << ": ";
+
+            Nodo* actual = adyacencia[i];
+
+            while(actual != nullptr) {
+                cout << actual->destino << " ";
+                actual = actual->siguiente;
+            }
+
+            cout << endl;
+        }
+    }
+
+    Nodo* obtenerLista(int edificio) {
+        return adyacencia[edificio];
+    }
+
+    int obtenerCantidadEdificios() {
+        return cantidadEdificios;
+    }
+
+    int* calcularGrado() {
+
+        int* grado = new int[cantidadEdificios + 1];
+
+        for(int i = 0; i <= cantidadEdificios; i++) {
+            grado[i] = 0;
+        }
+
+        for(int i = 1; i <= cantidadEdificios; i++) {
+
+            Nodo* actual = adyacencia[i];
+
+            while(actual != nullptr) {
+
+                grado[actual->destino]++;
+
+                actual = actual->siguiente;
+            }
+        }
+
+        return grado;
+    }
+
+    void imprimirGrado() {
+
+        int* grado = calcularGrado();
+
+        cout << endl;
+        cout << "Grado:" << endl;
+
+        for(int i = 1; i <= cantidadEdificios; i++) {
+
+            cout << "Edificio "
+                << i
+                << " -> "
+                << grado[i]
+                << endl;
+        }
+
+        delete[] grado;
+    }
+};
+
+        for(int i = 1; i <= cantidadEdificios; i++) {
+
+            cout << "Edificio "
+                << i
+                << " -> "
+                << grado[i]
+                << endl;
+        }
+
+        delete[] grado;
+    }
+};
+
+int main() {
+
+    int n;
+    int m;
+    int q;
+
+    cin >> n >> m >> q;
+
+    Grafo grafo(n);
+
+    for(int i = 1; i <= n; i++) {
+
+        int tiempo;
+        cin >> tiempo;
+
+        grafo.cambiarTiempo(i, tiempo);
+    }
+
+    for(int i = 0; i < m; i++) {
+
+        int a;
+        int b;
+
+        cin >> a >> b;
+
+        grafo.agregarArista(a, b);
+    }
+
+    grafo.imprimirGrafo();
+
+    grafo.imprimirGrado();
+
+    cout << "Lectura completada" << endl;
+
+    return 0;
+}
